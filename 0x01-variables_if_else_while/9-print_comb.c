c74e300f7b1ba05a145c5899dbd6ac070b50b858
Lyde2000 <lateleginia@gmail.com>
2023-06-18 10:42:26 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,32 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+
 /**
- * main - Prints the last digit of a randomly generated number and whether it is greater than 5, less than 6, or 0.
- *
- * Return: Always 0 (success)
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if ((n % 10) > 5)
-    {
-        printf("Last digit of %d is %d and is greater than 5\n",
-                n, n % 10);
-    }
-    else if ((n % 10) < 6)
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n",
-                n, n % 10);
-    }
-    else
-    {
-        printf("Last digit of %d is %d and is 0\n", n, n % 10);
-    }
-
-    return (0);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
