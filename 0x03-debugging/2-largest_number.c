8832472d24a0cbc1d1ab3990182ccf612e7d9003
Lyde2000 <lateleginia@gmail.com>
2023-06-22 14:58:03 -0700

    2-main

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,23 +10,18 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > c && b > a)
-    {
-        largest = b;
-    }
-    else if (c > b)
-    {
-        largest = c;
-    }
-    else
-    {
-    largest = b;
+int largest;
+if (a >= b && a >= c)
+{
+largest = a;
 }
-    return (largest);
+else if (b >= a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (largest);
 }
